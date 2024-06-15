e8d7d077f82a18512bf9ab45e5267e9c8abdbd5c
Gonzalo Gazzero <gonzagazzero2017@gmail.com>
2024-06-15 00:53:56 -0300

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,73 @@
-import gulp from 'gulp';
+const { src, dest, watch, parallel} = require("gulp")
 
-async function imgMinifyToWebp() { // función asíncrona
-  const imagemin = (await import('gulp-imagemin')).default; // Importación dinámica
-  const webp = (await import('gulp-webp')).default;        // Importación dinámica
+// css
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');//asegurarse q ande en cualquier navegador
+const cssnano = require('cssnano');//comprimir css
+const postcss = require('gulp-postcss');
 
-  return gulp.src('img/*')
-    .pipe(imagemin({ optimizationLevel: 5 }))
-    .pipe(webp())
-    .pipe(gulp.dest('img-webp'));
+// imagenes
+const avif = require('gulp-avif')
+const cache = require('gulp-cache')
+const webp= require('gulp-webp');
+const imagemin = require("gulp-imagemin");
+const sourcemaps = require('gulp-sourcemaps');
+
+//javascript
+const terser = require('gulp-terser-js');
+
+function css(done){
+    src('src/scss/**/*.scss')//identificar el archivo de sass
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe( sass() )//compilarlo
+        .pipe(postcss([autoprefixer(),cssnano()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/css'))//almacenarla en el disco duro
+    done();//callback q avisa a gulp cuando se llega al final de la ejecucion
 }
 
-export default imgMinifyToWebp;
+
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'))
+    done();
+}
+
+function versionwebp(done){
+
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+    done();
+}
+
+
+function javascript( done ) {
+    src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('build/js'));
+
+    done();
+}
+function dev(done){
+    watch('src/scss/**/*.scss',css)     //con el **/*.scss veo todos los cambios q hay con archivos de esa extension
+    watch('src/js/**/*.js',css)     //con el **/*.scss veo todos los cambios q hay con archivos de esa extension
+    done();
+}
+
+exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
+exports.versionwebp = versionwebp;
+exports.default = parallel(imagenes,versionwebp,javascript,dev);
